41d7e2b8be271b81755efd641606ba237c2e6314
woozink <k0109974@naver.com>
2023-06-25 17:08:44 +0900

    확장게임

---
+++ src/Bfs/불켜기.java
@@ -0,0 +1,96 @@
+package Bfs;
+
+import java.io.*;
+import java.util.*;
+/*
+각 방에서 상하좌우로 이동이 가능한데, 중요한 것은 불이  켜져있는 방으로만 들어갈 수 있다.
+-> 불을 켤 수 있는 방의 최대 개수
+
+-> 최대한 많이 이동하여 방을 방문해서 불을 많이 켜야한다.
+
+시작점은 무조건 불이 켜져있다. 개수 +1
+방문하지 못하는 방이더라도 불을 키면 해당 방을 count 할 수 있다. 개수 +1
+
+import 각 방에서 킬 수 있는 방은 인접한 방이 아니라 거리에 상관 없는 방도 가능하다.
+
+이는 지금까지 지나왔던 방들 중에 인접한 방에 불이 켜질 수도 있다.
+
+BFS 알고리ㅡㅈㅁ 중 지나 왔던 방이 없을 때 까지 1,1 에서 시작하는 BFS 알고리즘을 반복해서 수행한다.
+
+그래프를 그리기 위해 기존 문제들은 보통 하나의 Vertex에서 다른 하나의 Vertex로 이동하기 때문에 1차원 Arra
+ */
+
+
+public class 불켜기 {
+
+    static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static int n;
+    static boolean[][] switched, visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static ArrayList<Node>[][] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.valueOf(st.nextToken());
+        int m = Integer.valueOf(st.nextToken());
+        switched = new boolean[n][n]; // true : 불킴 / false : 불끔
+        visited = new boolean[n][n];    // 이미 방을 방문해서 불을 켰는지 안켰는지 체크
+
+        // 2차원 ArrayList 배열 사용
+        // 초기화
+        graph = new ArrayList[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = new ArrayList<Node>();
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            // 그래프 생성
+            Node endPoint = new Node(a, b);
+            graph[y][x].add(endPoint);
+
+            int count = bfs() + 1;
+
+            System.out.println(count);
+
+
+        }
+
+
+    }
+
+    private static int bfs() {
+        int count = 0;
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(0, 0));
+
+
+        // 방문 초기화
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(visited[i], false);
+        }
+
+
+
+
+        return 0;
+    }
+}
--- src/Bfs/확장게임.java
+++ src/Bfs/확장게임.java
@@ -1,11 +1,137 @@
 package Bfs;
+
 import java.io.*;
 import java.util.*;
 
 public class 확장게임 {
+    static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static int n, m, p;
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    static int[] player;
+    static int[][] map;
+
+    static Queue<Node> q[];
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        p = Integer.parseInt(st.nextToken());
+
+        // TODO 1. 초기화
+        player = new int[p + 1];
+        map = new int[n][m];
+
+        // TODO 2. 플레이어 별로 탐색할 큐 만들기
+        q = new LinkedList[p + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= p; i++) {
+            player[i] = Integer.parseInt(st.nextToken());
+            q[i] = new LinkedList<>();
+        }
+
+        // TODO 3. 입력 받기
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < m; j++) {
+                char tmp = str.charAt(j);
+
+                // map에 입력 된 것이 성이라면
+                if (tmp >= '1' && tmp <= '9') {
+                    map[i][j] = tmp - '0';
+                    q[map[i][j]].add(new Node(i, j));
+                }
+
+                // 벽이면 -1로 치환해서 넣기
+                else if (tmp == '#') {
+                    map[i][j] = -1;
+                }
+
+                // 갈 수 있는 통로면 0으로 치환해서 넣기
+                else if (tmp == '.') {
+                    map[i][j] = 0;
+                }
+            }
+        }
 
 
+        // TODO 4. SOL 시작
+        // TODO 5. BFS를 SIZE로 묶을 때 해당플레이어를 움질일 수 있을 만큼 반복
+        boolean flag = false;
+
+        while (!flag) {
+            // 플레이어 큐가 비면 종료를 시킨다.
+            int cnt = 0;
+
+            for (int i = 1; i <= p; i++) {
+                if (q[i].size() == 0) cnt++;
+            }
+
+            if (cnt == p) {
+                flag = true;
+                break;
+            }
+            bfs();
+        }
+
+        // TODO 7. 마지막 출력
+
+        int result[] = new int[p + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] >= 1 && map[i][j] <= 9) {
+                    result[map[i][j]]++;
+                }
+            }
+        }
+
+        for (int i = 1; i <= p; i++) {
+            System.out.print(result[i] + " ");
+        }
+
+    }
+
+    private static void bfs() {
+        for (int i = 1; i <= p; i++) {
+
+            if (q[i].isEmpty()) continue;
+
+            // 횟수 만큼 이동
+            for (int play = 0; play < player[i]; play++) {
+                int qsize = q[i].size();
+
+                if (qsize == 0) break;
+
+                while (qsize-- > 0) {
+                    Node node = q[i].poll();
+
+                    for (int dir = 0; dir < 4; dir++) {
+                        int nx = node.x + dx[dir];
+                        int ny = node.y + dy[dir];
+
+                        if (nx >= 0 && ny >= 0 && nx < n && ny <m && map[nx][ny] == 0) {
+                            map[nx][ny] = i;
+                            q[i].add(new Node(nx, ny));
+                        }
+                    }
+                }
+            }
+        }
     }
 }
