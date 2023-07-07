7b5a800c5ea650cc40d2a685ee188b25bb21bb58
woozink <77334903+woozink@users.noreply.github.com>
2023-07-07 20:15:03 +0900

    test

---
+++ src/시뮬레이션/치킨배달.java
@@ -0,0 +1,11 @@
+package 시뮬레이션;
+import java.io.*;
+import java.util.*;
+public class 치킨배달 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    }
+}
